0d0285e6f722b26bc01d3d9719e0e36fc0506d02
Michael Sergio <mikeserg@gmail.com>
2022-01-12 02:42:07 -0500

    Fixed addressing typo

--- graphics.asm
+++ graphics.asm
@@ -24,14 +24,14 @@ load_vram:
     phb  ; store bank
     php  ; store processor status registers
 
-    stx (CH0 + A1TxL)   ; DMA data offset
-    sta (CH0 + A1Bx)    ; DMA data bank
-    sty (CH0 + DASxL)   ; DMA size
+    stx CH0 + A1TxL   ; DMA data offset
+    sta CH0 + A1Bx    ; DMA data bank
+    sty CH0 + DASxL   ; DMA size
 
     lda #$01            ; DMA mode WORD (for VRAM 2-addr L,H)
-    sta (CH0 + DMAPx)
+    sta CH0 + DMAPx
     lda #$18            ; DMA destination register 2118 (VRAM data write)
-    sta (CH0 + BBADx)
+    sta CH0 + BBADx
 
     lda $01     ; DMA channel 0
     sta MDMAEN  ; Initiate transfer
@@ -61,14 +61,14 @@ dma_palette:
     phb  ; store bank
     php  ; store processor status registers
 
-    stx (CH0 + A1TxL)   ; DMA data offset
-    sta (CH0 + A1Bx)    ; DMA data bank
-    sty (CH0 + DASxL)   ; DMA size
+    stx CH0 + A1TxL   ; DMA data offset
+    sta CH0 + A1Bx    ; DMA data bank
+    sty CH0 + DASxL   ; DMA size
 
-    stz (CH0 + DMAPx)
+    stz CH0 + DMAPx
 
     lda #$22            ; DMA dest register - $2122
-    sta (CH0 + BBADx)
+    sta CH0 + BBADx
 
     lda #$01            ; DMA Channel 0
     sta MDMAEN          ; Initiate!
